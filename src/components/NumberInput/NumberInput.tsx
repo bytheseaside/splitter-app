96f0a68b1bc9fc012194ecf7e8d1b5f4fb4fb744
Brisa Rojas <brojas@solvd.com>
2023-01-11 13:37:24 -0300

    feat: number input component changes a give state (dropdown from parent) to have a track of the input value

--- src/components/NumberInput/NumberInput.tsx
+++ src/components/NumberInput/NumberInput.tsx
@@ -8,30 +8,32 @@ import {
   NumberIncrementStepper,
   NumberDecrementStepper
 } from '@chakra-ui/react'
-import { useState } from 'react'
 
 const NumericInput = ({
   icon,
   min,
   max,
   precision,
-  step
+  step,
+  value,
+  setValue
 }: {
   icon: any
   min: number
   max: number
   precision: number
   step: number
+  value?: number | undefined
+  setValue?: any
 }) => {
-  const [billAmount, setBillAmount] = useState(0)
-  const [numOfPeople, setNumOfPeople] = useState(0)
-
   return (
     <InputGroup size='lg' w='100%'>
       <InputLeftAddon>
         <Icon as={icon} />
       </InputLeftAddon>
       <NumberInput
+        value={value}
+        onChange={setValue}
         w='100%'
         min={min}
         max={max}
