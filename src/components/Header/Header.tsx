79e186b4b05d02ae1dfbe020527cf0889784bf5a
Brisa Rojas <brojas@solvd.com>
2023-01-11 21:06:47 -0300

    format: formatted file

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,12 @@ import { ExpandAltOutlined } from '@ant-design/icons'
 
 const Header = () => {
   return (
-    <Stack align='center' justify='center' direction={['column', 'column', 'row']} spacing={4}>
+    <Stack
+      align='center'
+      justify='center'
+      direction={['column', 'column', 'row']}
+      spacing={4}
+    >
       <Heading as='h2' size='2xl'>
         SPli
       </Heading>
