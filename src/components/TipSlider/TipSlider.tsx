2646e046e0b6e07962627fc22805fda135bff908
Brisa Rojas <brojas@solvd.com>
2023-01-11 21:05:29 -0300

    feat: slider for tip percentage
    made slider for tip percentage with a range of 0-100% and marks
    at some points for clarity to the user

---
+++ src/components/TipSlider/TipSlider.tsx
@@ -0,0 +1,70 @@
+import {
+  Center,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderMark,
+  Flex
+} from '@chakra-ui/react'
+import PercentageOutlined from '@ant-design/icons'
+
+const TipSlider = ({
+  sliderValue,
+  setSliderValue
+}: {
+  sliderValue: number
+  setSliderValue: Function
+}) => {
+  const labelStyles = {
+    mt: '2',
+    ml: '-2.5',
+    fontSize: 'sm'
+  }
+  return (
+    <Center py={3}  px={2}>
+      <Slider
+        aria-label='tip-percentage-selector'
+        step={1}
+        min={0}
+        max={100}
+        value={sliderValue}
+        onChange={(val) => setSliderValue(val)}
+        my={7}
+      >
+        <SliderMark value={25} {...labelStyles}>
+          25%
+        </SliderMark>
+        <SliderMark value={50} {...labelStyles}>
+          50%
+        </SliderMark>
+        <SliderMark value={75} {...labelStyles}>
+          75%
+        </SliderMark>
+        <SliderMark
+          value={sliderValue}
+          textAlign='center'
+          bg='blue.500'
+          color='white'
+          fontSize='sm'
+          mt='-10'
+          ml='-5'
+          px={1}
+          minW='12'
+          w='auto'
+        >
+          {sliderValue}%
+        </SliderMark>
+        <SliderTrack>
+          <SliderFilledTrack />
+        </SliderTrack>
+        <SliderThumb />
+      </Slider>
+      {/* <Flex ml={1} bg='blue.500' px={1} >
+       {sliderValue} %
+      </Flex> */}
+    </Center>
+  )
+}
+
+export default TipSlider
