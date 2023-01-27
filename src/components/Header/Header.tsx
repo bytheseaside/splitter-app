d4a4d12d05710b74bab13a2b2adcfe89ee0580b3
Brisa Rojas <brojas@solvd.com>
2023-01-27 14:29:51 -0300

    refactor: refactor header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,34 @@
-import { Heading, Stack, Icon } from '@chakra-ui/react'
+import { Heading, Stack, Icon, keyframes } from '@chakra-ui/react'
 import { ExpandAltOutlined } from '@ant-design/icons'
 
 const Header = () => {
+  const splitHorizontally = keyframes`
+  0% { width: auto; }
+  50% { width: 80%; }
+  100% { width: auto; }
+`
+  const splitVertically = keyframes`
+  from { height: 100%; }
+  to { height: 130%; }
+`
   return (
     <Stack
       align='center'
-      justify='center'
+      justify={['center', 'center', 'space-between']}
       direction={['column', 'column', 'row']}
       spacing={4}
+      mb={4}
+      sx={{
+        transition: 'all 0.35s ease-in-out',
+        '&:hover': {
+          animation: [
+            `${splitVertically} `,
+            `${splitVertically}`,
+            `${splitHorizontally}`
+          ],
+          textTransform: 'uppercase'
+        }
+      }}
     >
       <Heading as='h2' size='2xl'>
         SPli
